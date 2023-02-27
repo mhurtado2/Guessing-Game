e89262406e62b8cc04943cb39b24ae9be4be46fc
Shane Hurtado <mhurtado1@berklee.edu>
2023-02-27 10:08:32 -0600

    pase 1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+// namespace guessingGame
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+
+//make an app that invites user to guess a number
+// read that answer 
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Guess The Secret Number if you DARE!");
+    string answer = Console.ReadLine();
+    int parsedAnswer = int.Parse(answer);
+    Console.WriteLine(answer);
+}
