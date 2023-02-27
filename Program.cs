50bb0ddf7fed39523dbfdd8fd2a1e4fcd7ed4e9a
Shane Hurtado <mhurtado1@berklee.edu>
2023-02-27 10:49:11 -0600

    phase 3

--- Program.cs
+++ Program.cs
@@ -1,25 +1,59 @@
 ﻿using System;
 
-// namespace guessingGame
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             Console.WriteLine("Hello World!");
-//         }
-//     }
-// }
-
+//phase 1 
 //make an app that invites user to guess a number
 // read that answer 
+
+//phase 2
+// create a variable to contain secret number
+//42 is secret number
+// you condition statement to compare users guess to secret num
+
+
+//phase 3 give user 4 chances to guess
+
 
 Main();
 
 void Main()
 {
     Console.WriteLine("Guess The Secret Number if you DARE!");
-    string answer = Console.ReadLine();
-    int parsedAnswer = int.Parse(answer);
-    Console.WriteLine(answer);
+
+    int Guess()
+    {
+        int secretNum = 42;
+        string answer = Console.ReadLine();
+        int parsedAnswer = int.Parse(answer);
+
+
+        if (secretNum == parsedAnswer)
+        {
+            Console.WriteLine("You Are Correct!");
+        }
+        else
+        {
+            Console.WriteLine("You Are Wrong!");
+        }
+        return parsedAnswer;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        int try1 = Guess();
+        if (try1 == 42)
+        {
+            break;
+        }
+    }
+
+    // public class Guess
+    // {
+    //     public int Value { get; set; }
+    // }
+
+    // public Guess(int value)
+    // {
+    //     this.Value = value;
+    // }
+
 }
